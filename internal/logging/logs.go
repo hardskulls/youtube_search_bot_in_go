117b5b9c49241cc0d3ac702ba86e561523aa26cc
Alexander Lebedev <hardskulls@rambler.ru>
2023-08-12 21:57:45 +0300

    [::📟debug (internal/handlers/callback internal/logging/logs)::] Added logs.

--- internal/logging/logs.go
+++ internal/logging/logs.go
@@ -1,6 +1,7 @@
 package logging
 
 import (
+	"fmt"
 	"log"
 	"runtime"
 )
@@ -8,40 +9,105 @@ import (
 func LogVar[T any](variable T, msg string) {
 	_, file, line, _ := runtime.Caller(1) // Caller(1) for outer function, Caller(0) for this one.
 	separator := "------------------------------------------------------"
-	log.Printf("\n\n%v", separator)
-	log.Printf("\n\n[ LOG VAR Caller(1)] [ File and line: %v:%v | Message: '%v' ]", file, line, msg)
-	log.Printf("\n[ VAR ] Type is <| %T |> Value is [ <| %v |> ] ", variable, variable)
-	log.Printf("\n\n%v \n\n\n\n", separator)
+	emptyLine := "                                                      "
+
+	log.Println(emptyLine)
+
+	log.Println(separator)
+	log.Println(emptyLine)
+
+	log.Printf(fmt.Sprintf("[ LOG VAR Caller(1)] [ File and line: %v:%v | Message: '%v' ]", file, line, msg))
+	log.Printf(fmt.Sprintf("[ VAR ] Type is <| %T |> Value is [ <| %v |> ] ", variable, variable))
+
+	log.Println(emptyLine)
+	log.Println(separator)
+
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
 }
 
 func LogLine() {
 	_, file, line, _ := runtime.Caller(1) // Caller(1) for outer function, Caller(0) for this one.
 	separator := "------------------------------------------------------"
-	log.Printf("\n\n%v", separator)
-	log.Printf("\n\n[ LOG LINE ] [ Line <| %v:%v |> successfully executed. ]", file, line)
-	log.Printf("\n\n%v \n\n\n\n", separator)
+	emptyLine := "                                                      "
+
+	log.Println(emptyLine)
+
+	log.Println(separator)
+	log.Println(emptyLine)
+
+	log.Println(fmt.Sprintf("[ LOG LINE ] [ Line <| %v:%v |> successfully executed. ]", file, line))
+
+	log.Println(emptyLine)
+	log.Println(separator)
+
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
 }
 
 func LogError(err error) {
 	if err != nil {
 		_, file, line, _ := runtime.Caller(1) // Caller(1) for outer function, Caller(0) for this one.
 		separator := "------------------------------------------------------"
-		log.Printf("\n\n%v", separator)
-		log.Printf("\n\n[ ERROR ] : ( error is <| %v |> | file and line: <| %v:%v |> )", err, file, line)
-		log.Printf("\n\n%v \n\n\n\n", separator)
+		emptyLine := "                                                      "
+
+		log.Println(emptyLine)
+
+		log.Println(separator)
+		log.Println(emptyLine)
+
+		log.Println(fmt.Sprintf("[ ERROR ] : ( error is <| %v |> | file and line: <| %v:%v |> )", err, file, line))
+
+		log.Println(emptyLine)
+		log.Println(separator)
+
+		log.Println(emptyLine)
+		log.Println(emptyLine)
+		log.Println(emptyLine)
+		log.Println(emptyLine)
 	}
 }
 
 func LogFuncStart(name string) {
 	separator := "------------------------------------------------------"
-	log.Printf("\n\n%v", separator)
-	log.Printf("\n\n[ STARTED <| '%v' |>. ]", name)
-	log.Printf("\n\n%v \n\n\n\n", separator)
+	emptyLine := "                                                      "
+
+	log.Println(emptyLine)
+
+	log.Println(separator)
+	log.Println(emptyLine)
+
+	log.Println(fmt.Sprintf("[ STARTED <| '%v' |>. ]", name))
+
+	log.Println(emptyLine)
+	log.Println(separator)
+
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
 }
 
 func LogFuncEnd(name string) {
 	separator := "------------------------------------------------------"
-	log.Printf("\n\n%v", separator)
-	log.Printf("\n\n[ ENDED <| '%v' |>. ]", name)
-	log.Printf("\n\n%v \n\n\n\n", separator)
+	emptyLine := "                                                      "
+
+	log.Println(emptyLine)
+
+	log.Println(separator)
+	log.Println(emptyLine)
+
+	log.Println(fmt.Sprintf("[ ENDED <| '%v' |>. ]", name))
+
+	log.Println(emptyLine)
+	log.Println(separator)
+
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
+	log.Println(emptyLine)
 }
