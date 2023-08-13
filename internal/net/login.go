7114926959b635d69873cf6b3513962b57191715
Alexander Lebedev <hardskulls@rambler.ru>
2023-08-13 17:24:20 +0300

    [::🩹fix (internal/net/login)::] Fixed `STATE_CODE` in login link.

--- internal/net/login.go
+++ internal/net/login.go
@@ -1,18 +1,20 @@
 package net
 
 import (
-	"golang.org/x/oauth2"
+	"os"
 	"strconv"
 	config2 "youtube_search_go_bot/internal/config"
 	dialogue2 "youtube_search_go_bot/internal/dialogue"
 	"youtube_search_go_bot/internal/errors"
+
+	"golang.org/x/oauth2"
 )
 
 // Creates url for YouTube authentification.
 func LogInUrl(userId int64) string {
 	state := dialogue2.BuildKeyValueString(
 		[]dialogue2.KVStruct{
-			{Key: "state_code", Value: "kut987987_576fg78d5687lojfvkzr_85y6_435sgred_vnhgx_gdut"},
+			{Key: "state_code", Value: os.Getenv("STATE_CODE")},
 			{Key: "for_user", Value: strconv.FormatInt(userId, 10)},
 		},
 	)
